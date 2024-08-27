0272323a8cec19d61134cecaa9610ce04bcc656d
K S Saivaibhav <ks.saivaibhav@accolitedigital.com>
2024-08-27 16:07:09 +0530

    Initial changes - created react app installed parcel

---
+++ App.js
@@ -0,0 +1,13 @@
+import Body from "./components/Body";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
+
+export default App;
