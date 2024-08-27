26ffe112b83bb02591ccd9908761781562d15de8
K S Saivaibhav <ks.saivaibhav@accolitedigital.com>
2024-08-27 16:36:00 +0530

    added header section

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,24 @@
+import "./Header.css";
+
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <div className="header">
+      <div className="logo-div">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/042/642/799/non_2x/hand-drawn-empanada-logo-template-suitable-for-restaurant-and-cafe-vector.jpg"
+          alt="image"
+          className="logo-img"
+        />
+      </div>
+
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Products</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
