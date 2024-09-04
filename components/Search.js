a6dbf86914738f11e78be79898acced5b585cfb3
K S Saivaibhav <saivaibhavks2022@gmail.com>
2024-09-04 23:24:59 +0530

    search functionality

--- components/Search.js
+++ components/Search.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import "./Search.css";
 
 const Search = ({ restaurantData, setRestaurantData }) => {
+  const [inputSearch, setInputSearch] = useState("");
   const inputChange = (event) => {
     console.log("event", event);
   };
 
   return (
-    <div className="cont">
+    <div>
       <input
         type="text"
         placeholder="Enter"
-        value="search"
-        onChange={inputChange}
+        value={inputSearch}
+        onChange={(e) => {
+          const str = e.target.value.toLowerCase();
+          console.log("str", str);
+          const data = restaurantData.filter((item) => {
+            const name = item.info.name.toLowerCase();
+            console.log("name", name);
+            return name.includes(str);
+          });
+
+          console.log("data", data);
+          setInputSearch(e.target.value);
+          setRestaurantData(data);
+        }}
       />
     </div>
   );
